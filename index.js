db2a4396c98513af636a45bfac4cacd813ac701f
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-07-31 00:02:49 -0500

    song complete

---
+++ index.js
@@ -0,0 +1,77 @@
+console.log("Hello World!");
+
+function spinMeRound() {
+    console.log("You spin me right 'round, baby");
+    console.log("Right 'round like a record, baby");
+
+    console.log("Right 'round, 'round, 'round");
+    console.log("You spin me right 'round, baby");
+    console.log("Right 'round like a record, baby");
+    console.log("Right 'round, 'round, 'round");
+
+    console.log("If I, I get to know your name");
+    console.log("Well I could trace");
+
+    console.log("Your private number, baby");
+    console.log("All I know is that to me");
+    console.log("You look like you're lots of fun");
+    console.log("Open up your lovin' arms");
+
+    console.log("I want some, want some");
+    console.log("I've set my sights on you");
+    console.log("(Like no one else will do)");
+    console.log("And I, I've got to have");
+    console.log("My way now, baby");
+    console.log("All I know is that to me");
+
+    console.log("You look like you're havin' fun");
+    console.log("Open up your lovin' arms");
+
+    console.log("Watch out, here I come");
+    console.log("You spin me right 'round baby");
+    console.log("Right 'round like a record, baby");
+    console.log("Right 'round, 'round, 'round");
+
+    console.log("You spin me right 'round, baby");
+    console.log("Right 'round like a record, baby");
+
+    console.log("Right 'round, 'round, 'round");
+
+    console.log("I, I got to be your friend now, baby");
+
+    console.log("And I, I-I-I-I would like to move");
+    console.log("In just a little bit closer");
+    console.log("All I know is that to me");
+    console.log("You look like you're lots of fun");
+    console.log("Open up your lovin' arms");
+    console.log("Watch out, here I come");
+
+    console.log("You spin me right 'round, baby");
+    console.log("Right 'round like a record, baby");
+
+    console.log("Right 'round, 'round, 'round");
+    console.log("You spin me right 'round, baby");
+    console.log("Right 'round like a record, baby");
+    console.log("Right 'round, 'round, 'round");
+
+    console.log("I want your love");
+    console.log("I want your love");
+    console.log("All I know is that to me");
+    console.log("You look like you're lots of fun");
+    console.log("Open up your lovin' arms");
+    console.log("Watch out, here I come");
+    console.log("You spin me right 'round, baby");
+    console.log("Right 'round like a record, baby");
+    console.log("Right 'round, 'round, 'round");
+    console.log("You spin me right 'round, baby");
+    console.log("Right 'round like a record, baby");
+    console.log("Right 'round, 'round, 'round, 'round");
+    console.log("You spin me right 'round, baby");
+    console.log("Right 'round like a record, baby");
+    console.log("Right 'round, 'round, 'round");
+    console.log("You spin me right 'round, baby");
+    console.log("Right 'round like a record, baby");
+    console.log("Right 'round, 'round, 'round");
+
+}
+spinMeRound();
